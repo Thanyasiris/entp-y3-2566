8a45061e2cff4884ea870fe9aa7d88d3e098539f
Thanyasiri <63050146@kmitl.ac.th>
2023-02-02 00:55:40 +0700

    ngong

--- ex1/1/TCPClient.java
+++ ex1/1/TCPClient.java
@@ -1,17 +1,13 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 class TCPClient {
     public static void main(String[] args) throws Exception {
         String sentence;
         String modifiedSentence;
-        
-        System.out.println("Client\n");
-        System.out.println("enter number 1 (to end just press enter): ");
-        int num1 = 0;
-        if(num1 != null){
-            System.out.println("enter number 2 (to end just press enter): ");
-        }
+        int num1, num2;
+        Scanner sc = new Scanner(System.in);
 
         // Create input stream
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
@@ -25,15 +21,29 @@ class TCPClient {
         // Create input stream attached to socket
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        sentence = inFromUser.readLine();
+        System.out.println("Client\n");
+        System.out.println("enter number 1 (to end just press enter): ");
+        String input1 = inFromUser.readLine();
+        if (input1 != "") {
+            num1 = Integer.parseInt(input1);
+            System.out.println("enter number 2 (to end just press enter): ");
+            String input2 = inFromUser.readLine();
+            if (input2 != "") {
+                num2 = Integer.parseInt(input2);
 
-        // Sent line to server
-        outToServer.writeBytes(sentence + "\n");
+                // sentence = inFromUser.readLine();
 
-        // Read line from server
-        modifiedSentence = inFromServer.readLine();
+                // Sent line to server
+                outToServer.writeBytes(input1);
+                modifiedSentence = inFromServer.readLine();
+                outToServer.writeBytes(input2);
 
-        System.out.println("FROM SERVER: " + modifiedSentence);
+                // Read line from server
+                modifiedSentence = inFromServer.readLine();
+
+                System.out.println("FROM SERVER: " + modifiedSentence);
+            }
+        }
         clientSocket.close();
     }
 }
--- ex1/1/TCPServer.java
+++ ex1/1/TCPServer.java
@@ -5,6 +5,9 @@ class TCPServer {
     public static void main(String[] args) throws Exception {
         String clientSentence;
         String capitalizedSentence;
+        int prevNum = 0;
+        int num = 0;
+        int count = 0;
 
         System.out.println("Server\nWaiting for client connection at port number 1667");
 
@@ -18,11 +21,22 @@ class TCPServer {
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             // Create output stream, attached to socket
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
             // Read the line from socked
             clientSentence = inFromClient.readLine();
-            capitalizedSentence = "The result is " + clientSentence.toUpperCase() + "\n";
-            // Write out line to socket
-            outToClient.writeBytes(capitalizedSentence);
+            num = Integer.parseInt(clientSentence);
+            count++;
+            if (count == 1) {
+                prevNum = num;
+                outToClient.writeBytes("");
+            }
+            if (count == 2) {
+                capitalizedSentence = "The result is " + (prevNum + num) + "\n";
+                // Write out line to socket
+                outToClient.writeBytes(capitalizedSentence);
+                prevNum = 0;
+                count = 0;
+            }
         }
     }
 }
